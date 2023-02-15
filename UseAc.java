69378778bb4de85b0d96a022141a1c0137264a26
Arunkumar-1595 <asmarundurai1595@gmail.com>
2023-02-15 12:05:41 +0530

    AirConditioner

--- UseAc.java
+++ UseAc.java
@@ -3,7 +3,7 @@ package ac;
 public class UseAc {
 	public static void main(String[]args) {
 		Motor m1=new Motor();
-		m1.setBrand("Vitto");
+		m1.setBrand("Gen 3");
 		m1.setModel("V3");
 		m1.setCc(500);
 		
